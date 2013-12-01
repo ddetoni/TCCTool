436bd63a34cbbd4ad09817d4c71771437c5621d5
douglas <douglas_detoni@hotmail.com>
2013-12-01 18:47:43 -0200

    VSG refactored.

--- src/main.py
+++ src/main.py
@@ -11,11 +11,11 @@ from visual_student_graphic import VisualStudentGraphic
 
 if __name__ == '__main__':
     
-    #data = DataLoader().load_from_file('../data/logs_course_37.in')
+    data = DataLoader().load_from_file('../data/logs_course_37.in')
     #data = DataLoader().load_from_file('../data/name.in')
     #data = DataLoader().load_from_serial('../data/name.in.srz')
-    data = DataLoader().load_from_serial('../data/logs_course_37.in.srz')
-    
+    #data = DataLoader().load_from_serial('../data/logs_course_37.in.srz')
+    '''
     count = 0
     fig_name = 'fig_'
     
@@ -27,3 +27,10 @@ if __name__ == '__main__':
         vsg = VisualStudentGraphic(student)
         vsg.save_fig('../graphics/', fig_name+str(count))
         count += 1
+    '''
+    student = data.students[u"Natália Moreira Viana"]
+        
+    vsg = VisualStudentGraphic(student)
+    #vsg.plot()
+    vsg.save_fig('./','teste')
+    
--- src/visual_student_graphic.py
+++ src/visual_student_graphic.py
@@ -9,7 +9,6 @@ Created on 26/11/2013
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime as dt
-from Tkinter import *
 
 class VisualStudentGraphic:
     
@@ -18,20 +17,22 @@ class VisualStudentGraphic:
     def __init__(self, student):
         
         self.student = student
-
-    def plot(self):
         
         ticks = ['01/01','02/01','03/01','04/01','05/01','06/01','07/01','08/01','09/01','10/01','11/01','12/01']
         xticks = [dt.datetime.strptime(d,'%m/%d').date() for d in ticks]
         
         dates = self.student.interactions.keys()
+        
+        #Convert date format and add to list
         x = [dt.datetime.strptime(d,'%m/%d').date() for d in dates]
         x.sort()
         y = [self.student.interactions[d] for d in dates]
-    
+        
+        #Format x axis
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
         plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    
+        
+        #Set date points and interactions points
         plt.plot_date(x, y, '-', None, True)
         
         plt.xticks(xticks, rotation=80)
@@ -40,29 +41,12 @@ class VisualStudentGraphic:
         plt.xlabel('Day')
         plt.title(self.student.name + ' ' + self.student.last_name + ' - Interaction Graphic')
         
+    #Plot the graphic
+    def plot(self):
         plt.show()
     
+    # Save the graphic with format PNG
     def save_fig(self, save_path, save_name):
-        
-        ticks = ['01/01','02/01','03/01','04/01','05/01','06/01','07/01','08/01','09/01','10/01','11/01','12/01']
-        xticks = [dt.datetime.strptime(d,'%m/%d').date() for d in ticks]
-        
-        dates = self.student.interactions.keys()
-        x = [dt.datetime.strptime(d,'%m/%d').date() for d in dates]
-        x.sort()
-        y = [self.student.interactions[d] for d in dates]
-    
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    
-        plt.plot_date(x, y, '-', None, True)
-        
-        plt.xticks(xticks, rotation=80)
-        
-        plt.ylabel('Interaction number')
-        plt.xlabel('Day')
-        plt.title(self.student.name + ' ' + self.student.last_name + ' - Interaction Graphic')
-        
         final_path = save_path+save_name+".png"
         plt.savefig(final_path, format='png')
         plt.close()
