2dc41315af2f6b8d62d3b75fe672dcc00053394e
douglas <douglas_detoni@hotmail.com>
2013-12-03 00:11:57 -0200

    Modified gitignore.

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     #data = DataLoader().load_from_file('../data/name.in')
     #data = DataLoader().load_from_serial('../data/name.in.srz')
     #data = DataLoader().load_from_serial('../data/logs_course_37.in.srz')
-    '''
+   
     count = 0
     fig_name = 'fig_'
     
@@ -33,4 +33,4 @@ if __name__ == '__main__':
     vsg = VisualStudentGraphic(student)
     #vsg.plot()
     vsg.save_fig('./','teste')
-    
+    '''
