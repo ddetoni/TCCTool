9cc2c1a29fcbbbd1002ceb0e5735e5d4daa07b2a
douglas <douglas_detoni@hotmail.com>
2013-12-04 15:00:24 -0200

    Implemented methods normalize, save, load, prin on Semester class.

--- src/main.py
+++ src/main.py
@@ -12,13 +12,28 @@ from visual_student_graphic import VisualStudentGraphic
 
 if __name__ == '__main__':
     
+    '''
     semester = Semester("Semester_1")
     
-    course = DataLoader().load_from_file('../data/semester_1/logs_course_35.in', 'APE_I')
+    course0 = DataLoader().load_from_file('../data/semester_1/logs_course_35.in', 'APE_I')
+    course1 = DataLoader().load_from_file('../data/semester_1/logs_course_36.in', 'PE_I')
+    course2 = DataLoader().load_from_file('../data/semester_1/logs_course_37.in', 'EPP_I')
+    course3 = DataLoader().load_from_file('../data/semester_1/logs_course_38.in', 'EC_I')
+    course4 = DataLoader().load_from_file('../data/semester_1/logs_course_39.in', 'EAD_I')
     
-    print "Course Name: " + course.name
+    semester.courses[course0.name] = course0
+    semester.courses[course1.name] = course1
+    semester.courses[course2.name] = course2
+    semester.courses[course3.name] = course3
+    semester.courses[course4.name] = course4
     
+    semester.save("../data/semester_1/")
+    '''
     
+    semester = Semester('load').load('../data/semester_1/Semester_1.srz')
+    semester.normalize()
+    
+    semester.print_all_student_names()
     
     '''
     count = 0
--- src/semester.py
+++ src/semester.py
@@ -3,13 +3,64 @@ Created on 04/12/2013
 
 @author: douglas
 '''
+import pickle
 
 class Semester:
 
     courses = None
     name = None
+    normalize_students = None
 
     def __init__(self, name):
         
         self.courses = {}
-        name = name
+        self.name = name
+    
+    #This fuction remove the students that are not in all the courses.
+    def normalize(self):
+        
+        print "Normalizing semester..."
+        course_names = self.courses.keys();
+        
+        for c_name in course_names:
+
+            students = self.courses[c_name].students
+            student_names = students.keys()
+            
+            if self.normalize_students == None:
+                self.normalize_students = set(student_names)
+            else:
+                self.normalize_students = self.normalize_students.intersection(set(student_names))
+        
+        for c_name in course_names:
+            
+            students = self.courses[c_name].students
+            student_names = students.keys()
+            
+            diff_students = set(student_names).difference(self.normalize_students)
+            
+            while diff_students:
+                
+                s_name = diff_students.pop()
+                del self.courses[c_name].students[s_name]
+        
+        print "Semester normalized.\n"
+    
+    #Save a serialization of semester
+    def save(self, file_path):
+
+        pickle.dump(self, open(file_path + self.name + ".srz", 'wb'))
+    
+    #Load a serializantion
+    def load(self, srz_file_path):
+
+        self = pickle.load(open(srz_file_path, 'r'))
+        return self
+
+    def print_all_student_names(self):
+        
+        student_names = list(self.normalize_students)
+        student_names.sort()
+        
+        for name in student_names:
+            print name
